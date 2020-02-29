9f5d489ad72c0ff33a64fbbd8620d7a4fc8bb030
Tom <metaphorps@gmail.com>
2020-02-29 01:24:38 -0600

    ...

--- streams.js
+++ streams.js
@@ -1,1 +1,12 @@
-const stream = require('stream')
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+    // Solution 1
+    fs.readFile('test-file.txt', (err, data) => {
+        if (err) console.log(err);
+        res.end(data);
+    })
+
+    // Solution 2
+});
